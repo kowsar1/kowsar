df07b676ba0919935b17bf652cb878a42fa8a2cb
unknown <Sharmin22>
2022-10-10 23:16:38 +0600

    1st commit-Sharmin

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,6 +22,7 @@
     </head>
     <body >
           <p> ................................................hello kowsar</p>
+          <p> --------------I have cloned it properly bro...---------</p>
 
     </body>
 </html>
